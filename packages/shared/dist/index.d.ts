e1de01b0e0548c5db61825c9fd9194cc9aa44012
Mistaxe <captain.jennings@gmail.com>
2025-08-14 18:45:07 -0400

    Updated commit to remove node modules

--- packages/shared/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type { AuthorizationContext, ContextBuilder, AuthorizationDecision, PolicySimulationResult, ConsentResult } from './types/authorization.js';
-export { AuthorizationContextValidator } from './types/authorization.js';
-export * from './types/authorization.js';
--- packages/shared/dist/index.js
+++
@@ -1,3 +0,0 @@
-export { AuthorizationContextValidator } from './types/authorization.js';
-// Re-export common types that will be shared across the platform
-export * from './types/authorization.js';
